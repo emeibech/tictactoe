ab1beef0192ab43f08cecf0f6b7fd968c4c504c5
emeibech <emeibech@gmail.com>
2022-10-11 17:15:57 +0800

    Create player object factory and game flow module

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const gameObj = (() => {
-    const _gameboard = [];
+    const gameboard = [];
 
     //get the boxes' data attributes and push them to gameboard array
     const getBoxes = (() => {
         document.querySelectorAll('.gameboard > div')
-            .forEach(box => _gameboard.push(Number(box.getAttribute('data-box'))));
+            .forEach(box => gameboard.push(Number(box.getAttribute('data-box'))));
     })();
 
     //event: get input from client
@@ -18,25 +18,82 @@ const gameObj = (() => {
         const boxNumber = e.target.getAttribute('data-box');
 
         //check if target box's number is inside the gameboard array
-        const isActive = _gameboard.includes(Number(boxNumber));
+        const isActive = gameboard.includes(Number(boxNumber));
         
         //if target box's number is inside the array, remove it and replace with null
         if(isActive == true) {
-            _gameboard.splice(boxNumber, 1, null);
-            console.log(e.target)
+            gameboard.splice(boxNumber, 1, null);
+            console.log(e.target);
+            gameFlow.changeTurn();
+            console.log(player1.playerIsActive)
+            console.log(player2.playerIsActive)
         }
     }
 
     return {
         getBoxes,
-        _gameboard,
+        gameboard,
         getInput,
-        processInput
+        processInput,
     }
 })();
 
-const player = (playerName) => {
-    
+const player = () => {
+
+    let playerIsActive = null;
+
+    const markedBox = {
+        0: false,
+        1: false,
+        2: false,
+        3: false,
+        4: false,
+        5: false,
+        6: false,
+        7: false,
+        8: false,
+    }
+
+    const playerMove = (e) => {
+        console.log(e.target.getAttribute('data-box'));
+    }
+
+    return {
+        playerIsActive,
+        markedBox,
+        playerMove,
+    }
 }
+
+//create players
+const player1 = player();
+const player2 = player();
+
+const gameFlow = (() => {
+    
+    //set player turn
+    const setTurn = (() => {
+        player1.playerIsActive = true;
+        player2.playerIsActive = false;
+    })();
+
+    //swicth turn when board is marked
+    const changeTurn = () => {
+        const nullCounter = gameObj.gameboard.filter(e => e == null);
+        if(!(nullCounter.length % 2 == 0)) {
+            console.log('hey')
+            player1.playerIsActive = false;
+            player2.playerIsActive = true;
+        }
+        if(nullCounter.length % 2 == 0) {
+            player1.playerIsActive = true;
+            player2.playerIsActive = false;
+        }
+    }
+
+    return {
+        changeTurn,
+    }
+})();
 
 gameObj.getInput();
